c051d43a337feaf0efd84d160a0490141c44dd7d
gary <github@accu.ca>
2023-11-01 09:25:20 -0700

    Add Counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import 'App.css';
+import Counter from 'Counter';
 
 function App() {
 
   return (
     <div className="App">
-      <h2>
-        Hello React World
-      </h2>
+      <h2>Hello React World</h2>
+      <Counter />
     </div>
   );
 }
